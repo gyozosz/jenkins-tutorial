c16a3c52637b45617364066c0db74b36a30fbd23
gyozo.szabo <gyozo.szabo@nixs.com>
2023-11-22 11:53:37 +0100

    letsgo

---
+++ myapp/hello.py
@@ -0,0 +1,12 @@
+import requests
+from hamcrest import assert_that, equal_to
+
+
+def hello(name):
+    print(f"Hello {name}!")
+
+
+if __name__ == "__main__":
+    hello("James")
+    response = requests.request('GET', "en.wikipedia.org")
+    assert_that(response.status_code, equal_to(200))
